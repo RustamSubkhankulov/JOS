97da0ee5ef5b56825afd0599559ac8e5a331e748
Vitaly Cheptsov <cheptsov@ispras.ru>
2022-12-29 14:09:13 +0300

    lab5: fix compilation with llvm 15

--- inc/random.h
+++ inc/random.h
@@ -5,7 +5,7 @@
 
 extern int rand(void);
 extern void srand(unsigned int seed);
-extern void rand_init();
+extern void rand_init(unsigned int num);
 extern unsigned char _dev_urandom[];
 extern unsigned int _dev_urandom_len;
 

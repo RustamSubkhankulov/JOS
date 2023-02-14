14d949d872f553657024fdde544d044a65f56f7b
Rustam Subkhankulov <subkhankulov@ispras.ru>
2023-02-14 11:45:30 +0300

    mistake in comment corrected

--- inc/mmu.h
+++ inc/mmu.h
@@ -110,7 +110,7 @@
 #define CR0_EM 0x00000004 /* Emulation */
 #define CR0_TS 0x00000008 /* Task Switched */
 #define CR0_ET 0x00000010 /* Extension Type */
-#define CR0_NE 0x00000020 /* Numeric Errror */
+#define CR0_NE 0x00000020 /* Numeric Error */
 #define CR0_WP 0x00010000 /* Write Protect */
 #define CR0_AM 0x00040000 /* Alignment Mask */
 #define CR0_NW 0x20000000 /* Not Writethrough */

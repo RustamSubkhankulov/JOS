9d45be14b25b802dc299fe993536a79234306913
Rustam Subkhankulov <subkhankulov@ispras.ru>
2023-04-18 18:47:49 +0300

    lab 12 changes

--- kern/trap.c
+++ kern/trap.c
@@ -2,6 +2,7 @@
 #include <inc/x86.h>
 #include <inc/assert.h>
 #include <inc/string.h>
+#include <inc/vsyscall.h>
 
 #include <kern/pmap.h>
 #include <kern/trap.h>
@@ -13,6 +14,7 @@
 #include <kern/kclock.h>
 #include <kern/picirq.h>
 #include <kern/timer.h>
+#include <kern/vsyscall.h>
 #include <kern/traceopt.h>
 
 static struct Taskstate ts;
@@ -336,6 +338,7 @@ trap_dispatch(struct Trapframe *tf) {
     case IRQ_OFFSET + IRQ_CLOCK:
         // LAB 5: Your code here
         // LAB 4: Your code here
+        // LAB 12: Your code here
         {
             // rtc_timer_pic_handle();
 
